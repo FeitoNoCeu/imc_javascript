906081e276034357dd0ab82cb5416908b396688f
FeitoNoCeu <89472795+FeitoNoCeu@users.noreply.github.com>
2021-11-22 16:25:59 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,26 @@
+function calcula_imc(){
+    altura = document.imcform.altura.value
+    peso = document.imcform.peso.value
+    imc = (peso/(altura*altura))
+
+    if(imc<18.5){
+        alert("Você está abaixo do peso, índice: "+ imc)
+    }
+
+    else if(imc>=18.5 && imc <= 24.99){
+        alert("Você está com o peso ideal, indice: "+ imc)
+    }
+
+    else if(imc>=25 && imc <= 29.99){
+        alert("Você está com sobrepeso, indice: "+ imc)
+    }
+
+    else if(imc>=30 && imc <= 39.99){
+        alert("Você está com obesidade, indice: "+ imc)
+    }
+
+    else if(imc>=40){
+        alert("Você está com obesidade mórbida, indice: "+ imc)
+    }
+
+}
